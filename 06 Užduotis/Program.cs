b2b26af4e6c34ee83b40da81e88214999da6f56b
Kotryna Meškauskaitė <meskauskaite.k@gmail.com>
2023-03-11 12:49:37 +0200

    ideta 6 uzduotis

---
+++ 06 Užduotis/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Užduotis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Išveskite pasirinkto skaičiaus daugybos lentelę.
+            int skaicius = 8;
+            int daugiklis = 0;
+            Console.WriteLine($"{skaicius} * {daugiklis} = {skaicius * daugiklis}");
+            Console.WriteLine($"{skaicius} * {++daugiklis} = {skaicius * daugiklis}");
+            Console.WriteLine($"{skaicius} * {++daugiklis} = {skaicius * daugiklis}");
+            Console.WriteLine($"{skaicius} * {++daugiklis} = {skaicius * daugiklis}");
+            Console.WriteLine($"{skaicius} * {++daugiklis} = {skaicius * daugiklis}");
+            Console.WriteLine($"{skaicius} * {++daugiklis} = {skaicius * daugiklis}");
+            Console.WriteLine($"{skaicius} * {++daugiklis} = {skaicius * daugiklis}");
+            Console.WriteLine($"{skaicius} * {++daugiklis} = {skaicius * daugiklis}");
+            Console.WriteLine($"{skaicius} * {++daugiklis} = {skaicius * daugiklis}");
+            Console.WriteLine($"{skaicius} * {++daugiklis} = {skaicius * daugiklis}");
+            Console.WriteLine($"{skaicius} * {++daugiklis} = {skaicius * daugiklis}");
+
+            //Susikurkite sveikojo skaičiaus kintamąjį su dviženklio skaičiaus reikšme. Išveskite į ekraną šio skaičiaus skaitmenų sandaugą.
+            int number = 55;
+            Console.WriteLine($"{number} * {number} = {number *= number}");
+
+
+        }
+    }
+}
