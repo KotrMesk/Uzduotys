3d33ac7a185a790f8d1ff1a3c0b7f7c060e43e34
Kotryna Meškauskaitė <meskauskaite.k@gmail.com>
2023-03-11 12:01:31 +0200

    Add project files.

---
+++ 01 Užduotis/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Užduotis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Parašykite programą, kuri išvestų žodį “Labas” ir jūsų vardą toje pačioje eilutėje.
+            Console.Write("Įveskite savo vardą: ");
+            string vardas = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine($"Labas, {vardas}");
+            Console.WriteLine();
+
+            /*Susikurkite kintamąjį žmogaus amžiui nusakyti, išveskite į ekraną tekstą su šiuo amžiumi: “Įvestas amžius: { amžius}”. 
+            *Tarp { } nurodytas kintamasis, kurio reikšmė toje vietoje turi būti įstatyta.*/
+
+            int amzius = 35;
+            Console.WriteLine($"Įvestas amžius: {amzius}");
+            Console.WriteLine();
+
+            //Susikurkite skaičiaus kintamąjį, kurį išveskite penkis kartus toje pačioje eilutėje be tarpų tarp šių skaičių(skaičius -25, išvedimas - 2525252525).
+
+            string skaicius = "25";
+            Console.WriteLine(skaicius + skaicius + skaicius + skaicius + skaicius);
+
+            //Susikurkite skaičiaus kintamąjį, kurį išveskite penkis kartus toje pačioje eilutėje su tarpais tarp šių skaičių(skaičius -25, rezultatas - 25 25 25 25 25).
+
+            
+            Console.WriteLine("{0} {0} {0} {0} {0}", skaicius);
+
+        }
+    }
+}
---
+++ 02 Užduotis/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Užduotis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Įveskite savo vardą: ");
+            String vardas = Console.ReadLine();
+            string linija = new string('-', vardas.Length);
+            string tarpas = new string(' ', vardas.Length);
+            Console.WriteLine($"---{linija}---");
+            Console.WriteLine($"|  {tarpas}  |");
+            Console.WriteLine($"|  {vardas}  |");
+            Console.WriteLine($"|  {tarpas}  |");
+            Console.WriteLine($"---{linija}---");
+        }
+    }
+}
