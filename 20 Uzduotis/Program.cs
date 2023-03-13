18c73a6c6180f7f442d8d5fac0f55ddf4f873658
Kotryna Meškauskaitė <meskauskaite.k@gmail.com>
2023-03-13 19:07:57 +0200

    atlikta 20 uzduotis

---
+++ 20 Uzduotis/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_Uzduotis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*Įveskite iš konsolės 2 skaičius (stataus trikampio statinių ilgius).
+             * Suraskite įžambinę ir ją išveskite į ekraną.
+             */
+            Console.Write("Iveskite pirma stataus trikampio statinio ilgi: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Iveskite antra stataus trikampio statinio ilgi: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Trikampio izambine: {Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2))}");
+
+        }
+    }
+}
