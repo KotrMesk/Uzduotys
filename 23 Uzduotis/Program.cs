bc2d88fe36a8be61bf3275dab8ba20a2c41032b9
Kotryna Meškauskaitė <meskauskaite.k@gmail.com>
2023-03-13 22:51:25 +0200

    atlikta 23 uzduotis

---
+++ 23 Uzduotis/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_Uzduotis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*Susikurkite kintamąjį, kuris pasakytų kiek automobilis vidutiniškai sunaudoja  degalų 100 – ui km. 
+             * Susikurkite masyvą, kuriame saugotumėte skirtingose kelionėse nuvažiuotų kilometrų rodmenis. Raskite:
+             * trumpiausia kelione
+             * ilgiausia kelione
+             * kiek kuro sanaudu sunaudojo automobilis (skaičiavimui reikės rasti visus nuvažiuotus km).
+             */
+
+            double degaluSanaudos = 11;
+            int[] keliones = { 100, 240, 80, 530, 1200 };
+            int min = keliones.Min();
+            int max = keliones.Max();
+            double sanaudosKelioniu = keliones.Sum() * degaluSanaudos / 100;
+            Console.WriteLine($"Trumpiausia kelione: {min}");
+            Console.WriteLine($"Ilgiausia kelione: {max}");
+            Console.WriteLine($"Visu kelioniu kuro sanaudos: {sanaudosKelioniu}");
+        }
+    }
+}
