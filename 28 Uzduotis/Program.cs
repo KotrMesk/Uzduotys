faa03f393d49964cf4f4177b0c2c21dfe8a7a015
Kotryna Meškauskaitė <meskauskaite.k@gmail.com>
2023-03-14 18:23:53 +0200

    Atlikta 28 uzduotis

---
+++ 28 Uzduotis/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Deployment.Internal;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28_Uzduotis
+{
+    internal class Zodziai
+    {
+        internal static void Main(string[] args)
+        {
+            //Aprašykite ir iškvieskite metodą, kuris išvestų bet kokį jūsų pasirinktą tekstą.
+            Pasisveikinimas();
+            Pasisveikinimas();
+            Pasisveikinimas();
+            Atsisveikinimas();
+
+
+        }
+
+        private static void Pasisveikinimas()
+        {
+            Console.WriteLine("Labas");
+        }
+
+        private static void Atsisveikinimas()
+        {
+            Console.WriteLine("Ate");
+        }
+    }
+}
