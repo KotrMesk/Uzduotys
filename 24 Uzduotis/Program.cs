80707e977aee9b801ef31bfe7590edecf4306dfe
Kotryna Meškauskaitė <meskauskaite.k@gmail.com>
2023-03-14 16:54:25 +0200

    atlikta 24 uzduotis

--- 24 Uzduotis/Program.cs
+++ 24 Uzduotis/Program.cs
@@ -17,10 +17,39 @@ namespace _24_Uzduotis
              * Suma1 – kai masyvo elementas yra lyginis skaičius
              * Suma2 – kai masyvo indeksas nėra lyginis skaičius
              */
+            Console.WriteLine("Kiek duomenu nori ivesti i masyva?");
+            int kiek = Convert.ToInt32(Console.ReadLine());
 
-            
+            int[] skaiciai = new int[kiek];
 
-            
+            for (int i = 0; i < skaiciai.Length; i++)
+            {
+                Console.WriteLine("Iveskite skaiciu: ");
+                skaiciai[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int suma1 = 0;
+
+            foreach (int skaicius in skaiciai)
+            {
+                if (skaicius % 2 == 0)
+                {
+                    suma1 += skaicius;
+                }
+
+            }
+            Console.WriteLine($"Lyginiu skaiciu suma: {suma1}");
+
+            int suma2 = 0;
+
+            for (int i = 0; i < skaiciai.Length; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    suma2 += skaiciai[i];
+                }
+            }
+            Console.WriteLine($"Nelyginiu skaiciu suma: {suma2}");
         }
     }
 }
