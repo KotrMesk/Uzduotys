5115b30e8d1f8f644a5ef8ca52fe6bfbd722e77c
Kotryna Meškauskaitė <meskauskaite.k@gmail.com>
2023-03-15 20:32:28 +0200

    Atlikta 38 uzduotis

---
+++ 38.2 Uzduotis/Gyvunas.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _38._2_Uzduotis
+{
+    class Gyvunas
+    {
+        protected string Pavadinimas { get; set; }
+        protected int Amzius { get; set; }
+
+        private const string Rusis = "Zinduolis";
+        public Gyvunas(string Pavadinimas, int Amzius)
+        {
+            this.Pavadinimas = Pavadinimas;
+            this.Amzius = Amzius;
+
+        }
+
+        public virtual void Informacija()
+        {
+            Console.WriteLine($"Gyvunas {Pavadinimas} ({Amzius} m.)");
+            
+        }
+        public virtual void Zinduolis()
+        {
+            Console.WriteLine($"Gyvunas {Pavadinimas} ({Amzius} m.), {Rusis}");
+
+        }
+    }
+    class Gyvate : Gyvunas
+    {
+        protected string spalva { get; set; }
+
+        public Gyvate(string Pavadinimas, int Amzius, string spalva) : base(Pavadinimas, Amzius)
+        {
+            this.Pavadinimas = Pavadinimas;
+            this.Amzius = Amzius;
+            this.spalva = spalva;
+        }
+
+        public override void Informacija()
+        {
+            base.Zinduolis();
+            Console.WriteLine($"Spalva: {spalva}");
+        }
+    }
+}
---
+++ 38.2 Uzduotis/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _38._2_Uzduotis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*nusikopijuokite praeitos užduoties kodą į naują projektą.
+             * Base klasėje parašykite virtualų išvedimo metodą.
+             * Paveldinčioje klasėje pakeiskite esamą išvedimo metodą, kad jis override‘intu base klasės virtual metodą.
+             * Base klasėje sukurkite private const kintamąjį, kadangi jo negalima pasiekti  už klasės ribų – base klasėje sukurkite public metodą, 
+             * kuris išspausdina  private const kintamąjį. Panaudoję šį metodą, iškvieskite jį paveldinčios klasės išvedimo metode.
+             */
+
+            Gyvunas gyvunas1 = new Gyvunas("Suo", 3);
+            Gyvate gyvunas2 = new Gyvate("Gyvate", 1, "melyna");
+
+            gyvunas2.Informacija();
+
+
+        }
+    }
+}
