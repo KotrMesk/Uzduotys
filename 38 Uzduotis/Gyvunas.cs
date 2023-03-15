cc9c9cd18c6722ea022adc0f7d658fcd77539295
Kotryna Meškauskaitė <meskauskaite.k@gmail.com>
2023-03-15 20:18:44 +0200

    Atlikta 38 Uzduotis

---
+++ 38 Uzduotis/Gyvunas.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Lifetime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _38_Uzduotis
+{
+    class Gyvunas
+    {
+        protected string Pavadinimas { get; set; }
+        protected int Amzius { get; set; }
+        public Gyvunas(string Pavadinimas, int Amzius)
+        {
+            this.Pavadinimas = Pavadinimas;
+            this.Amzius = Amzius;
+            
+        }
+    }
+    class Gyvate : Gyvunas
+    {
+        protected string spalva { get; set; }
+
+        public Gyvate(string Pavadinimas, int Amzius, string spalva) : base(Pavadinimas, Amzius) 
+        {
+            this.Pavadinimas = Pavadinimas;
+            this.Amzius = Amzius;
+            this.spalva = spalva;
+        }
+
+        internal void Informacija()
+        {
+            Console.WriteLine($"Gyvunas {Pavadinimas} ({Amzius} m.)");
+            Console.WriteLine($"Spalva: {spalva}");
+        }
+    }
+}
---
+++ 38 Uzduotis/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _38_Uzduotis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Sukurkite bazinę klasę Gyvunas su protected kintamaisiais ir paveldinčia klasę Gyvate. 
+             * Paveldinčioje klasėje turi būti metodas, kuris išveda visus klasės  kintamuosius į ekraną.
+             * Bazinės klasės konstruktorių panaudokite paveldinčioje klasėje.
+             * Išveskite paveldinčios klasės kintamuosius į ekraną.
+             */
+
+            Gyvunas gyvunas1 = new Gyvunas("Suo", 3);
+            Gyvate gyvunas2 = new Gyvate("Gyvate", 1, "melyna");
+           
+            gyvunas2.Informacija();
+
+        }
+    }
+}
